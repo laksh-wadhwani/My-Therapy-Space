72fcc5920b32cdc8ecfe79e13e4e71faf23cdc94
laksh-wadhwani-trusoft <laksh.wadhwani@trusoft.pk>
2025-07-23 23:33:49 +0500

    Completed Our team Page

--- frontend/src/Screens/Team.jsx
+++ frontend/src/Screens/Team.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../Components/footer"
 import Team1 from "../assets/team1.png"
 import Team2 from "../assets/team2.png"
 import Team3 from "../assets/team3.png"
@@ -36,7 +37,7 @@ const Team = () => {
 
     return(
         <React.Fragment>
-            <div className="main-box">
+            <div className="main-box gap-20">
 
                 <div className="w-full flex flex-col gap-8 px-14 mt-32 box-border">
                     <div>
@@ -60,16 +61,56 @@ const Team = () => {
 
                 </div>
 
-                {/* <div className="w-full flex flex-col gap-20 px-14">
+                <div className="w-full flex flex-col gap-8 px-14">
                     <h2 className="font-serif text-3xl text-[#0BAFA6] capitalize">join our team</h2>
-                    <div className="w-full flex gap-16">
+
+                    <div className="w-full flex gap-16 max-h-[520px]">
                         <div className="flex flex-col items-start justify-between">
-                            <p className="font-serif text-2xl text-justify text-black">If you are an allied health professional who is passionate about working with children and teenagers then My Therapy Space is for you.  We provide a flexible, supportive, multi disciplinary team experience.  We are driven to provide the best possible service for families that is family centred, and driven by families.  Staff are offered a flexible working environment, with regular supervision/mentoring and professional development. If this resonates with you, then please contact us to discuss further.</p>
+                            <p className="font-serif text-xl text-justify text-black font-light leading-[3.25rem]">If you are an allied health professional who is passionate about working with children and teenagers then My Therapy Space is for you.  We provide a flexible, supportive, multi disciplinary team experience.  We are driven to provide the best possible service for families that is family centred, and driven by families.  Staff are offered a flexible working environment, with regular supervision/mentoring and professional development. If this resonates with you, then please contact us to discuss further.</p>
                             <RButton className="px-7 py-3">Apply Now</RButton>
                         </div>
-                        <img src={Team1} alt="" />
+                        <img src={Team1} alt="" className="object-cover rounded-xl" />
                     </div>
-                </div> */}
+
+                    <div className="w-full flex gap-16 mt-8 max-h-[643px]">
+                        <img src={Team2} alt="" className="object-cover rounded-xl" />
+                        <div className="flex flex-col items-start gap-4">
+                            <strong className="font-serif text-2xl text-justify text-black">What Makes My Therapy Space Different?</strong>
+                            <p className="font-serif text-xl text-justify text-black font-light leading-[2.5rem]">Since opening a Speech Pathology private practice in 2001, directors Trish and Lisa have dreamt about forming the perfect multi disciplinary team. In 2018 that dream came true and My Therapy Space was born! Seven of our staff members had worked for between 5 & 20 years at a government organisation together. Our team is a wonderful combination of fabulous people working closely as a multi disciplinary team that supports children with all types of presenting difficulties, as well as teenagers and young adults with disabilities. We provide Speech Pathology, Occupational Therapy, and Dietician services.<br/>The directors live and breathe our company and work alongside all the therapists daily on the ground. There is no corporate structure here! Only people who truly care for their staff and the families we support. They work hard to recruit exactly the right people that will fit with our company culture of being fun, innovative and caring.</p>
+                        </div>
+                    </div>
+
+                    <div className="w-full flex gap-16 mt-8 max-h-[682px]">
+                        <div className="flex flex-col items-start gap-4">
+                            <strong className="font-serif text-2xl text-justify text-black">New Graduate? Only Worked a Year in Pediatrics?</strong>
+                            <p className="font-serif text-xl text-justify text-black font-light leading-[2.75rem]">If you’re a new graduate or early career clinician in your first two years of working in paediatrics, we have a specialised program for you: .We know it’s daunting entering the workforce as an allied health new graduate, especially going into a private practice. Our company is passionate about nurturing our early career clinicians, by having a well developed new graduate program that helps you ease into the working world. We do this by a slow build up of your caseload, weekly supervision, a second weekly support session and training program, on tap access to directors to support you, a generous professional development budget, reasonable client numbers of 20 hours per week so adequate time for research, planning and thinking. The majority of our sessions are within the clinic so you’re not spending a lot of time travelling and you will have access to a wonderful experienced team. New graduates will also have the other new grad members on the team for moral support!</p>
+                        </div>
+                        <img src={Team3} alt="" className="object-cover rounded-xl" />
+                    </div>
+
+                </div>
+
+                <div className="w-full flex flex-col gap-8 px-14 pb-12 items-center">
+                    <p className="font-serif text-3xl text-black self-start">Benefits and Perks for All Staff</p>
+                    <ul className="list-disc pl-5 font-serif text-lg text-black flex flex-col gap-4 text-justify">
+                        <li>A wonderful, supportive and fun team of 17 therapists who genuinely care for each other.</li>
+                        <li>6 senior clinicians with over 20 years experience each.</li>
+                        <li>A detailed onboarding program where you will be allowed the time to fully understand the clinic and our process. Depending on experience, each clinician will gradually build up your client caseload while you build confidence in working with our procedures.</li>
+                        <li>Realistic expectations for billable hours for all staff (at 20 hours per week for staff in first two years of graduating, building up to a maximum of 25 hours after 10 years experience). This includes direct sessions with clients, report writing and travel.</li>
+                        <li>Fortnightly staff meetings and interaction with your team to brainstorm ideas.</li>
+                        <li>Shared multi disciplinary sessions and observation sessions to learn from each other.</li>
+                        <li>Beautiful brand new offices with extensive resources, equipment and toys.</li>
+                        <li>Next door to shops, restaurants and public transport.</li>
+                        <li>Flexible working hours, we know you and your family come first. There are options for 8am-4pm days, or 10am-6pm days and all the options in between! Some staff work slightly longer each day and have an RDO once a month.</li>
+                        <li>Above award pay rates and bonuses</li>
+                        <li>Free access to an “employee assistance program” which provides Psychology counselling for work or life challenges.</li>
+                        <li>A generous Professional Development allowance including 3 paid days annually to attend training.</li>
+                        <li>Regular social events as a team</li>
+                    </ul>
+                    <RButton className="px-7 py-3">Join Now</RButton>
+                </div>
+
+                <Footer/>
 
             </div>
         </React.Fragment>
