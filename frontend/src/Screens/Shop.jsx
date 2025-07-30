7784344d47342ed69dfd006e4396912555e88060
laksh-wadhwani-trusoft <laksh.wadhwani@trusoft.pk>
2025-07-30 22:08:55 +0500

    Completed Shops Page

--- frontend/src/Screens/Shop.jsx
+++ frontend/src/Screens/Shop.jsx
@@ -14,15 +14,24 @@ const Shop = () => {
                     <p className="font-serif text-black text-xl capitalize">Welcome to My Therapy Space Shopping Area Please Select one of the below categories</p>
                 </div>
 
-                <div className="w-full flex justify-center">
+                <div className="w-full flex justify-between px-60">
 
-                    <div className="w-[40%] box-border p-6 bg-white shadow-md flex flex-col items-center gap-6">
-                        <img src={ShopProducts} alt="Shop Products Picture" className="w-full h-80 object-contain" />
-                        <div className="w-[50%] flex flex-col items-center">
+                    <div className="max-w-[30rem] max-h-[34rem] box-border p-6 border border-black-100 bg-white shadow-xl rounded-t-[3rem] flex flex-col items-center gap-6">
+                        <img src={ShopProducts} alt="Shop Products Picture" className="w-full h-[60%] object-cover" />
+                        <div className="flex flex-col items-center">
                             <h3 className="font-serif text-xl text-black capitalize">shop products</h3>
                             <p className="text-lg font-serif text-gray-500 capitalize text-center">visual time tables, song boards, and therapy-related tools</p>
                         </div>
                         <RButton className="px-8 py-2">View Products</RButton>
+                    </div>
+
+                    <div className="max-w-[30rem] max-h-[34rem] box-border p-6 border border-black-100 bg-white shadow-xl rounded-t-[3rem] flex flex-col items-center gap-6">
+                        <img src={ShopVideos} alt="Shop Products Picture" className="w-full h-[60%] object-cover" />
+                        <div className="flex flex-col items-center">
+                            <h3 className="font-serif text-xl text-black capitalize">Shop Course Videos</h3>
+                            <p className="text-lg font-serif text-gray-500 capitalize text-center">Online Course Videos, Guided-therapy related videos</p>
+                        </div>
+                        <RButton className="px-8 py-2">View Courses</RButton>
                     </div>
 
                 </div>
