370411387a607b18f1023320f278e07962dfe100
laksh-wadhwani <laksh-wadhwani55@gmail.com>
2025-08-09 05:54:31 +0500

    Added forget password and otp verification

--- admin/src/Screens/Login.jsx
+++ admin/src/Screens/Login.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginBg from "../assets/loginbg.png";
 import CustomInput from "../Components/CustomInput";
 import CustomButton from "../Components/CustomButton";
 import Navbar from "../Components/navbar";
 
 const Login = () => {
+
+    const [forgetPassToggle, setForgetPassToggle] = useState(false)
+
     return (
         <React.Fragment>
 
@@ -13,18 +16,32 @@ const Login = () => {
             <div className="w-full h-dvh bg-left bg-no-repeat [background-size:120%] absolute flex items-center px-52" style={{ backgroundImage: `url(${LoginBg})` }}>
 
                 <div className="w-100 p-12 bg-white border border-gray-200 shadow-md rounded-xl flex flex-col gap-14 mt-16">
-                    <h2 className="font-serif capitalize text-2xl text-black font-semibold self-center">admin login</h2>
-                    
-                    <div className="w-full flex flex-col gap-4">
-                        <CustomInput label="Email" type="email" placeholder="Email" />
-                        <CustomInput label="Password" type="password" placeholder="Password"/>
-                        <span className="font-serif text-base text-black capitalize self-end">forget password?</span>
-                    </div>
 
-                    <div className="flex flex-col gap-2">
-                        <CustomButton>Login</CustomButton>
-                        <p className="font-serif text-base text-black self-center">Don't have an account? <a href="/" className="text-[#00C7BE] font-bold">Create</a></p>
-                    </div>
+                    <h2 className="font-serif capitalize text-2xl text-black font-semibold self-center">{forgetPassToggle? `forget password` : `admin login`}</h2>
+
+                    {forgetPassToggle? 
+                    (
+                    <>
+                        <CustomInput label="Enter registered email" type="email" placeholder="Email"/>
+                        <CustomButton>Forget Password</CustomButton>
+                    </>
+                    )
+                    :
+                    (
+                        <>
+                         <div className="w-full flex flex-col gap-4">
+                            <CustomInput label="Email" type="email" placeholder="Email" />
+                            <CustomInput label="Password" type="password" placeholder="Password"/>
+                            <span className="font-serif text-base text-black capitalize underline cursor-pointer self-end" onClick={() => setForgetPassToggle(true)}>forget password?</span>
+                        </div>   
+
+                        <div className="flex flex-col gap-2">
+                            <CustomButton>Login</CustomButton>
+                            <p className="font-serif text-base text-black self-center">Don't have an account? <a href="/" className="text-[#00C7BE] font-bold">Create</a></p>
+                        </div>
+                        </>
+                    )
+                    }
                 </div>
 
             </div>
--- admin/src/Screens/Signup.jsx
+++ admin/src/Screens/Signup.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Components/navbar";
 import CustomInput from "../Components/CustomInput";
 import CustomButton from "../Components/CustomButton";
 import LoginBg from "../assets/loginbg.png";
 
 const Signup = () => {
+
+    const [otpToggle, setOtpToggle] = useState(false)
+
     return(
         <React.Fragment>
             <Navbar/>
             <div className="w-full h-dvh bg-left bg-no-repeat [background-size:120%] absolute flex items-center px-52" style={{ backgroundImage: `url(${LoginBg})` }}>
 
                 <div className="w-100 py-8 px-12 bg-white border border-gray-200 shadow-md rounded-xl flex flex-col gap-8 mt-16">
-                    <h2 className="font-serif capitalize text-2xl text-black font-semibold self-center">admin signup</h2>
-                    
+                    <h2 className="font-serif capitalize text-2xl text-black font-semibold self-center">{otpToggle? `Verification` : `admin signup`}</h2>
+
+                    {otpToggle? 
+                    <>
+                    <div className="flex justify-between">
+                        <input type="text" className="p-2 border border-black rounded-xl size-14 text-center" />
+                        <input type="text" className="p-2 border border-black rounded-xl size-14 text-center" />
+                        <input type="text" className="p-2 border border-black rounded-xl size-14 text-center" />
+                        <input type="text" className="p-2 border border-black rounded-xl size-14 text-center" />
+                    </div>
+                    <CustomButton onClick={() => setOtpToggle(false)}>Verify</CustomButton>
+                    </> 
+                    : 
+                    <>
                     <div className="w-full flex flex-col gap-3">
                         <CustomInput label="Name" type="text" placeholder="Name"/>
                         <CustomInput label="Email" type="email" placeholder="Email" />
@@ -21,9 +36,11 @@ const Signup = () => {
                     </div>
 
                     <div className="flex flex-col gap-2">
-                        <CustomButton>Create Account</CustomButton>
+                        <CustomButton onClick={() => setOtpToggle(true)}>Create Account</CustomButton>
                         <p className="font-serif text-base text-black self-center">Already have an account? <a href="/" className="text-[#00C7BE] font-bold">Login</a></p>
                     </div>
+                    </>
+                    }
                 </div>
 
             </div>
