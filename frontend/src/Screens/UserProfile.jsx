6fdbb78d860e9d1127b1e702657afb52f7d72b32
laksh-wadhwani <laksh-wadhwani55@gmail.com>
2025-09-14 16:44:51 +0500

    Responsive User Profile

--- frontend/src/Screens/UserProfile.jsx
+++ frontend/src/Screens/UserProfile.jsx
@@ -132,78 +132,85 @@ const UserProfile = ({ setLoginUser }) => {
     return (
         <React.Fragment>
 
-            <div className="fixed px-8 w-full flex flex-col gap-2 items-center">
+            <div className="w-full flex flex-col lg:flex-row gap-12 px-8 pb-4 mt-16 md:mt-24 lg:mt-28">
 
-                <div className={`w-full flex flex-col gap-4 items-center ${user.profile ? `rounded-xl bg-white border border-gray-100 shadow-lg p-2` : ``}`}>
-                    {user.profile ?
-                        <img src={user.profile} alt="User Profile" className="w-full h-60 object-contain" />
+                <div className="w-full lg:w-[27%] xl:w-[20%] lg:fixed lg:top-24 flex flex-col gap-2 items-center">
+
+                    <div className={`w-full flex flex-col gap-2 items-center ${user.profile ? `rounded-xl bg-white border border-gray-100 shadow-lg p-2` : ``}`}>
+                        {user.profile ?
+                            <img src={user.profile} alt="User Profile" className="w-full h-60 object-contain" />
+                            :
+                            <div className="w-full h-60 bg-[#16171D] rounded-xl flex justify-center items-center shadow-lg">
+                                <UserRound size={200} className="fill-gray-500 stroke-none" />
+                            </div>
+                        }
+                    </div>
+
+                    {isUpdate ?
+                        <CustomButton onClick={() => setOpen(true)}>{user.profile ? `Change Photo` : `Upload Photo`}</CustomButton>
                         :
-                        <div className="w-full h-60 bg-[#16171D] rounded-xl flex justify-center items-center shadow-lg">
-                            <UserRound size={200} className="fill-gray-500 stroke-none" />
-                        </div>
-                    }
+                        <>
+                            <CustomButton onClick={() => setIsUpdate(true)}>Manage Profile</CustomButton>
+                            <CustomButton onClick={Logout}>Logout</CustomButton>
+
+
+                            <div className="w-full flex flex-col gap-4 text-gray-500 font-serif bg-white border border-gray-100 shadow-lg rounded-xl box-border p-4">
+                                <h5 className="text-xl lg:text-lg xl:text-xl capitalize italic">personal information</h5>
+                                <div className="w-full flex flex-col text-base lg:text-xs xl:text-base gap-1">
+                                    <span><strong>Name:</strong> {user.fullname}</span>
+                                    <span><strong>Email:</strong> {user.email}</span>
+                                    <span><strong>Phone No:</strong> {user.phoneNo}</span>
+                                    <span><strong>Address:</strong> {user.address}</span>
+                                </div>
+                            </div>
+                        </>}
                 </div>
 
-                {isUpdate ?
-                    <CustomButton onClick={() => setOpen(true)}>{user.profile ? `Change Photo` : `Upload Photo`}</CustomButton>
-                    :
-                    <>
-                        <CustomButton onClick={() => setIsUpdate(true)}>Manage Profile</CustomButton>
-                        <CustomButton onClick={Logout}>Logout</CustomButton>
 
+                <div className="w-full lg:w-[73%] xl:w-[80%] lg:ml-[30%] xl:ml-[22%] flex flex-col gap-4 md:gap-8">
 
-                        <div className="w-full flex flex-col gap-4 text-gray-500 font-serif bg-white border border-gray-100 shadow-lg rounded-xl box-border p-4">
-                            <h5 className="text-xl capitalize italic">personal information</h5>
-                            <div className="w-full flex flex-col text-base gap-1">
-                                <span><strong>Name:</strong> {user.fullname}</span>
-                                <span><strong>Email:</strong> {user.email}</span>
-                                <span><strong>Phone No:</strong> {user.phoneNo}</span>
-                                <span><strong>Address:</strong> {user.address}</span>
+                    {isUpdate ?
+                        <>
+                            <div className="w-full flex flex-col gap-6">
+                                <h3 className="font-serif text-2xl italic">Personal Information</h3>
+                                <div className="w-full flex flex-col gap-4">
+                                    <CustomInput label="Full Name" type="text" placeholder={user.fullname} value={userData.fullname} onChange={e => handleChange("fullname", e.target.value)} />
+                                    <CustomInput label="Email" type="email" placeholder={user.email} value={userData.email} onChange={e => handleChange("email", e.target.value)} />
+                                    <CustomInput label="Phone Number" type="text" placeholder={user.phoneNo} value={userData.phoneNo} onChange={e => handleChange("phoneNo", e.target.value)} />
+                                    <CustomInput label="Address" type="text" placeholder={user.address} value={userData.address} onChange={e => handleChange("address", e.target.value)} />
+                                </div>
                             </div>
-                        </div>
-                    </>}
+
+                            <div className="w-full flex flex-col gap-6">
+                                <h3 className="font-serif text-2xl italic">Password</h3>
+                                <div className="w-full flex flex-col gap-4">
+                                    <CustomInput label="Current Password" placeholder="Current Password" type="password" value={userData.currentPass} onChange={e => handleChange("currentPass", e.target.value)} />
+                                    <CustomInput label="New Password" type="password" placeholder="New Password" showPasswordRules={true} value={userData.newPass} onChange={e => handleChange("newPass", e.target.value)} />
+                                </div>
+                            </div>
+
+                            <div className="w-full flex flex-col gap-4 justify-between">
+                                <CustomButton className="w-[30%] bg-blue-500" onClick={() => Update(user._id)} disabled={loading}>
+                                    {loading ? <div className="w-5 h-5 border-2 border-t-transparent border-black rounded-full animate-spin" /> : "Save"}
+                                </CustomButton>
+                                <CustomButton className="w-[30%] bg-red-500" onClick={() => setIsUpdate(false)}>Cancel</CustomButton>
+                            </div>
+                        </>
+                        :
+                        <>
+                            <h2 className="font-serif text-2xl italic text-gray-500 max-sm:text-center">Welcome back, <strong className="text-[#0BAFA6]">{user.fullname}</strong></h2>
+
+                            {bookingDetails.length===0? <p className="font-serif capitalize max-sm:text-center text-xl">There are no bookings</p>
+                            : 
+                            <CustomTable title="Booking History" columns={Bookingcolumns} data={bookingDetails} showActions={false} statusStyles={bookingStatusStyles} />}
+
+                            {orderDetails.length===0? <p className="font-serif capitalize max-sm:text-center text-xl">There are no purchases</p> : 
+                            <CustomTable title="Order History" columns={Ordercolumns} data={orderDetails} showActions onView={SeeDetails} />}
+                        </>
+                    }
+
+                </div>
             </div>
-
-
-            {/* <div className="w-[78%] max-sm:w-full flex flex-col gap-8 pb-4 mt-28 px-6 float-right">
-
-                {isUpdate ?
-                    <>
-                        <div className="w-full flex flex-col gap-6">
-                            <h3 className="font-serif text-3xl max-sm:text-2xl italic">Personal Information</h3>
-                            <div className="w-[75%] max-sm:w-full flex flex-col gap-4">
-                                <CustomInput label="Full Name" type="text" placeholder={user.fullname} value={userData.fullname} onChange={e => handleChange("fullname", e.target.value)} />
-                                <CustomInput label="Email" type="email" placeholder={user.email} value={userData.email} onChange={e => handleChange("email", e.target.value)} />
-                                <CustomInput label="Phone Number" type="text" placeholder={user.phoneNo} value={userData.phoneNo} onChange={e => handleChange("phoneNo", e.target.value)} />
-                                <CustomInput label="Address" type="text" placeholder={user.address} value={userData.address} onChange={e => handleChange("address", e.target.value)} />
-                            </div>
-                        </div>
-
-                        <div className="w-full flex flex-col gap-6">
-                            <h3 className="font-serif text-3xl max-sm:text-2xl italic">Password</h3>
-                            <div className="w-[75%] max-sm:w-full flex flex-col gap-4">
-                                <CustomInput label="Current Password" placeholder="Current Password" type="password" value={userData.currentPass} onChange={e => handleChange("currentPass", e.target.value)} />
-                                <CustomInput label="New Password" type="password" placeholder="New Password" showPasswordRules={true} value={userData.newPass} onChange={e => handleChange("newPass", e.target.value)} />
-                            </div>
-                        </div>
-
-                        <div className="w-[75%] max-sm:w-full flex max-sm:flex-col max-sm:gap-4 justify-between">
-                            <CustomButton className="w-[30%] bg-blue-500" onClick={() => Update(user._id)} disabled={loading}>
-                                {loading ? <div className="w-5 h-5 border-2 border-t-transparent border-black rounded-full animate-spin" /> : "Save"}
-                            </CustomButton>
-                            <CustomButton className="w-[30%] bg-red-500" onClick={() => setIsUpdate(false)}>Cancel</CustomButton>
-                        </div>
-                    </>
-                    :
-                    <>
-                        <h2 className="font-serif text-2xl max-sm:text-xl italic text-gray-500">Welcome back, <strong className="text-[#0BAFA6]">{user.fullname}</strong></h2>
-
-                        <CustomTable title="Booking History" columns={Bookingcolumns} data={bookingDetails} showActions={false} statusStyles={bookingStatusStyles} />
-                        <CustomTable title="Order History" columns={Ordercolumns} data={orderDetails} showActions onView={SeeDetails} />
-                    </>
-                }
-
-            </div> */}
 
 
             <Modal open={open} onClose={() => setOpen(false)} center
